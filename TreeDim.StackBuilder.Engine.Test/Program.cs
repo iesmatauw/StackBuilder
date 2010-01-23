d8597324cbdc592f2b9684ce18354551bfda68cb
fgasnier71_cp <SND\fgasnier71_cp>
2010-01-23 05:52:35 -0800

    + First implementation of StackBuilder engine.

---
+++ TreeDim.StackBuilder.Engine.Test/Program.cs
@@ -0,0 +1,23 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Text;
+#endregion
+
+namespace TreeDim.StackBuilder.Engine.Test
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return 0;
+        }
+    }
+}

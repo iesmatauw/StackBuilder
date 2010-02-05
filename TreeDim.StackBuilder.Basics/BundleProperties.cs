5cc939743a0ce5b9e97e7bbcf169c42bf8900b5b
fgasnier71_cp <SND\fgasnier71_cp>
2010-02-05 07:05:22 -0800

    + Added BundleProperties.cs and Interlayer.cs

---
+++ TreeDim.StackBuilder.Basics/BundleProperties.cs
@@ -0,0 +1,43 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+#endregion
+
+namespace TreeDim.StackBuilder.Basics
+{
+    public class BundleProperties : ItemProperties
+    {
+        #region Data members
+        private double _length = 0.0, _width = 0.0, _unitThickness = 0.0, _unitWeight = 0.0;
+        private int _noFlats;
+        private Color _color;
+        #endregion
+
+        #region Constructor
+        public BundleProperties(string name, string description,
+            double length, double width
+            , double unitThickness
+            , double unitWeight
+            , int noFlats
+            , Color color)
+            : base(name, description)
+        {
+            _length = length;
+            _width = width;
+            _unitThickness = unitThickness;
+            _unitWeight = unitWeight;
+            _noFlats = noFlats;
+        }
+        #endregion
+
+        #region Public properties
+        public Color Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+        #endregion
+    }
+}
---
+++ TreeDim.StackBuilder.Graphic/Interlayer.cs
@@ -0,0 +1,15 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+#endregion
+
+namespace TreeDim.StackBuilder.Graphics
+{
+    public class Interlayer
+    {
+        #region Data members
+        #endregion
+    }
+}

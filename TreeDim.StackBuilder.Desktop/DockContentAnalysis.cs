c3040aebfda84c05d8e369022d83467d1593c592
fgasnier71_cp <SND\fgasnier71_cp>
2010-06-24 01:52:20 -0700

    + Added Wix setup project + Changed assembly version to 0.1.0.0

---
+++ TreeDim.StackBuilder.Desktop/DockContentAnalysis.cs
@@ -0,0 +1,333 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+using log4net;
+
+using Sharp3D.Math.Core;
+using TreeDim.StackBuilder.Basics;
+using TreeDim.StackBuilder.Graphics;
+#endregion
+
+namespace TreeDim.StackBuilder.Desktop
+{
+    public partial class DockContentAnalysis : DockContent, IView
+    {
+        #region Data members
+        /// <summary>
+        /// document
+        /// </summary>
+        private IDocument _document;
+        /// <summary>
+        /// analysis
+        /// </summary>
+        private Analysis _analysis;
+        /// <summary>
+        /// view parameters
+        /// </summary>
+        private const double _cameraDistance = 10000.0;
+        /// <summary>
+        /// Currently selected solution
+        /// </summary>
+        private Solution _sol;
+        /// <summary>
+        /// logger
+        /// </summary>
+        static readonly ILog _log = LogManager.GetLogger(typeof(DockContentAnalysis));
+        #endregion
+
+        #region Constructor
+        public DockContentAnalysis(IDocument document, Analysis analysis)
+        {
+            _document = document;
+            _analysis = analysis;
+
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Form override
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            // Text
+            this.Text = _analysis.Name + " - " + _analysis.ParentDocument.Name;
+
+            // fill grid solution
+            gridSolutions.Rows.Clear();
+
+            // border
+            DevAge.Drawing.BorderLine border = new DevAge.Drawing.BorderLine(Color.DarkBlue, 1);
+            DevAge.Drawing.RectangleBorder cellBorder = new DevAge.Drawing.RectangleBorder(border, border);
+
+            // views
+            CellBackColorAlternate viewNormal = new CellBackColorAlternate(Color.LightBlue, Color.White);
+            viewNormal.Border = cellBorder;
+
+            // column header view
+            SourceGrid.Cells.Views.ColumnHeader viewColumnHeader = new SourceGrid.Cells.Views.ColumnHeader();
+            DevAge.Drawing.VisualElements.ColumnHeader backHeader = new DevAge.Drawing.VisualElements.ColumnHeader();
+            backHeader.BackColor = Color.LightGray;
+            backHeader.Border = DevAge.Drawing.RectangleBorder.NoBorder;
+            viewColumnHeader.Background = backHeader;
+            viewColumnHeader.ForeColor = Color.White;
+            viewColumnHeader.Font = new Font("Arial", 10, FontStyle.Bold);
+
+            // create the grid
+            gridSolutions.BorderStyle = BorderStyle.FixedSingle;
+
+            gridSolutions.ColumnsCount = 6;
+            gridSolutions.FixedRows = 1;
+            gridSolutions.Rows.Insert(0);
+
+            // header
+            SourceGrid.Cells.ColumnHeader columnHeader;
+
+            columnHeader = new SourceGrid.Cells.ColumnHeader("Index");
+            columnHeader.View = viewColumnHeader;
+            gridSolutions[0, 0] = columnHeader;
+
+            columnHeader = new SourceGrid.Cells.ColumnHeader("Layer pattern(s)");
+            columnHeader.View = viewColumnHeader;
+            gridSolutions[0, 1] = columnHeader;
+
+            columnHeader = new SourceGrid.Cells.ColumnHeader("Box count");
+            columnHeader.View = viewColumnHeader;
+            gridSolutions[0, 2] = columnHeader;
+
+            columnHeader = new SourceGrid.Cells.ColumnHeader("Efficiency (%)");
+            columnHeader.View = viewColumnHeader;
+            gridSolutions[0, 3] = columnHeader;
+
+            columnHeader = new SourceGrid.Cells.ColumnHeader("Pallet weight (kg)");
+            columnHeader.View = viewColumnHeader;
+            gridSolutions[0, 4] = columnHeader;
+
+            columnHeader = new SourceGrid.Cells.ColumnHeader("Pallet height (mm)");
+            columnHeader.View = viewColumnHeader;
+            gridSolutions[0, 5] = columnHeader;
+
+            // data rows
+            int iIndex = 0;
+            foreach (Solution sol in _analysis.Solutions)
+            {
+                ++iIndex;
+                gridSolutions.Rows.Insert(iIndex);
+                gridSolutions[iIndex, 0] = new SourceGrid.Cells.Cell(string.Format("{0}", iIndex));
+                {
+                    Graphics2DImage graphics = new Graphics2DImage(new Size(100, 50));
+                    SolutionViewer sv = new SolutionViewer(_analysis, sol);
+                    sv.Draw(graphics);
+                    gridSolutions[iIndex, 1] = new SourceGrid.Cells.Image(graphics.Bitmap);
+                }
+                gridSolutions[iIndex, 2] = new SourceGrid.Cells.Cell(string.Format("{0}", sol.BoxCount));
+                gridSolutions[iIndex, 3] = new SourceGrid.Cells.Cell(string.Format("{0:F}", sol.Efficiency(_analysis)));
+                gridSolutions[iIndex, 4] = new SourceGrid.Cells.Cell(string.Format("{0:F}", sol.PalletWeight(_analysis)));
+                gridSolutions[iIndex, 5] = new SourceGrid.Cells.Cell(string.Format("{0:F}", sol.PalletHeight(_analysis)));
+
+                gridSolutions[iIndex, 0].View = viewNormal;
+                gridSolutions[iIndex, 1].View = viewNormal;
+                gridSolutions[iIndex, 2].View = viewNormal;
+                gridSolutions[iIndex, 3].View = viewNormal;
+                gridSolutions[iIndex, 4].View = viewNormal;
+                gridSolutions[iIndex, 5].View = viewNormal;
+            }
+            gridSolutions.AutoStretchColumnsToFitWidth = true;
+            gridSolutions.AutoSizeCells();
+            gridSolutions.Columns.StretchToFit();
+
+            gridSolutions.Selection.SelectionChanged += new SourceGrid.RangeRegionChangedEventHandler(onGridSolutionSelectionChanged);
+
+            // select first solution
+            gridSolutions.Selection.SelectRow(1, true);
+        }
+
+        private void onGridSolutionSelectionChanged(object sender, SourceGrid.RangeRegionChangedEventArgs e)
+        {
+            SourceGrid.RangeRegion region = gridSolutions.Selection.GetSelectionRegion();
+            int[] indexes = region.GetRowsIndex();
+            // no selection -> exit
+            if (indexes.Length == 0) return;
+            // get selected solution
+            _sol = _analysis.Solutions[indexes[0] - 1];
+            // redraw
+            Draw();
+        }
+
+        private void pictureBoxSolution_SizeChanged(object sender, EventArgs e)
+        {
+            // redraw
+            Draw();
+        }
+        #endregion
+
+        #region IView implementation
+        public IDocument Document
+        {
+            get { return _document; }
+        }
+        #endregion
+
+        #region Public properties
+        public Analysis Analysis
+        {
+            get { return _analysis; }
+        }
+        #endregion
+
+        #region Drawing
+        private void Draw()
+        {
+            try
+            {
+                // sanity check
+                if (pictureBoxSolution.Size.Width < 1 || pictureBoxSolution.Size.Height < 1)
+                    return;
+                // instantiate graphics
+                Graphics3DImage graphics = new Graphics3DImage(pictureBoxSolution.Size);
+                // set camera position 
+                double angleHorizRad = trackBarAngleHoriz.Value * Math.PI / 180.0;
+                double angleVertRad = trackBarAngleVert.Value * Math.PI / 180.0;
+                graphics.CameraPosition = new Vector3D(
+                    _cameraDistance * Math.Cos(angleHorizRad) * Math.Cos(angleVertRad)
+                    , _cameraDistance * Math.Sin(angleHorizRad) * Math.Cos(angleVertRad)
+                    , _cameraDistance * Math.Sin(angleVertRad));
+                // set camera target
+                graphics.Target = new Vector3D(0.0, 0.0, 0.0);
+                // set light direction
+                graphics.LightDirection = new Vector3D(-0.75, -0.5, 1.0);
+                // set viewport (not actually needed)
+                graphics.SetViewport(-500.0f, -500.0f, 500.0f, 500.0f);
+
+                // instantiate solution viewer
+                SolutionViewer sv = new SolutionViewer(_analysis, _sol);
+                sv.Draw(graphics);
+
+                // show generated bitmap on picture box control
+                pictureBoxSolution.Image = graphics.Bitmap;
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.ToString());
+            }
+        }
+        #endregion
+
+        #region Handlers to define point of view
+        private void onAngleHorizChanged(object sender, EventArgs e)
+        {
+            trackBarAngleHoriz.Value = trackBarAngleHoriz.Value;
+            Draw();
+        }
+
+        private void onAngleVertChanged(object sender, EventArgs e)
+        {
+            trackBarAngleVert.Value = trackBarAngleVert.Value;
+            Draw();
+        }
+        private void onViewSideFront(object sender, EventArgs e)
+        {
+            trackBarAngleHoriz.Value = 0;
+            trackBarAngleVert.Value = 0;
+            Draw();
+        }
+
+        private void onViewSideLeft(object sender, EventArgs e)
+        {
+            trackBarAngleHoriz.Value = 90;
+            trackBarAngleVert.Value = 0;
+            Draw();
+        }
+
+        private void onViewSideRear(object sender, EventArgs e)
+        {
+            trackBarAngleHoriz.Value = 180;
+            trackBarAngleVert.Value = 0;
+            Draw();
+        }
+
+        private void onViewSideRight(object sender, EventArgs e)
+        {
+            trackBarAngleHoriz.Value = 270;
+            trackBarAngleVert.Value = 0;
+            Draw();
+        }
+
+        private void onViewCorner_0(object sender, EventArgs e)
+        {
+            trackBarAngleHoriz.Value = 45 + 0;
+            trackBarAngleVert.Value = 45;
+            Draw();
+        }
+
+        private void onViewCorner_90(object sender, EventArgs e)
+        {
+            trackBarAngleHoriz.Value = 45 + 90;
+            trackBarAngleVert.Value = 45;
+            Draw();
+        }
+
+        private void onViewCorner_180(object sender, EventArgs e)
+        {
+            trackBarAngleHoriz.Value = 45 + 180;
+            trackBarAngleVert.Value = 45;
+            Draw();
+        }
+
+        private void onViewCorner_270(object sender, EventArgs e)
+        {
+            trackBarAngleHoriz.Value = 45 + 270;
+            trackBarAngleVert.Value = 45;
+            Draw();
+        }
+
+        private void onViewTop(object sender, EventArgs e)
+        {
+            trackBarAngleHoriz.Value = 0;
+            trackBarAngleVert.Value = 90;
+            Draw();
+        }
+        #endregion
+
+        #region CellBackColorAlternate
+        private class CellBackColorAlternate : SourceGrid.Cells.Views.Cell
+        {
+            public CellBackColorAlternate(Color firstColor, Color secondColor)
+            {
+                FirstBackground = new DevAge.Drawing.VisualElements.BackgroundSolid(firstColor);
+                SecondBackground = new DevAge.Drawing.VisualElements.BackgroundSolid(secondColor);
+            }
+
+            private DevAge.Drawing.VisualElements.IVisualElement mFirstBackground;
+            public DevAge.Drawing.VisualElements.IVisualElement FirstBackground
+            {
+                get { return mFirstBackground; }
+                set { mFirstBackground = value; }
+            }
+
+            private DevAge.Drawing.VisualElements.IVisualElement mSecondBackground;
+            public DevAge.Drawing.VisualElements.IVisualElement SecondBackground
+            {
+                get { return mSecondBackground; }
+                set { mSecondBackground = value; }
+            }
+
+            protected override void PrepareView(SourceGrid.CellContext context)
+            {
+                base.PrepareView(context);
+
+                if (Math.IEEERemainder(context.Position.Row, 2) == 0)
+                    Background = FirstBackground;
+                else
+                    Background = SecondBackground;
+            }
+        }
+        #endregion
+    }
+}

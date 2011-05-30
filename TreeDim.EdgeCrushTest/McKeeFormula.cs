3abe093e30f05f14fb835ece557b52bb5991595b
fgasnier71_cp <SND\fgasnier71_cp>
2011-05-30 16:31:40 -0700

    + Added EdgeCrushTest project...

---
+++ TreeDim.EdgeCrushTest/McKeeFormula.cs
@@ -0,0 +1,28 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#endregion
+
+namespace TreeDim.EdgeCrushTest
+{
+    public class McKeeFormula
+    {
+        #region Formula implementation
+        public void FormulaOption1(double L, double B, double H, double T, double ECT, double kHumid, double kJStock, ref double k)
+        {
+            Formula(L / 1000.0, B / 1000.0, H / 1000.0, T / 1000.0, ECT, kHumid, kJStock, ref k);
+            k = k * 1000 / 9.81;    //kN -> kg
+        }
+
+        public void Formula(double L, double B, double H, double T, double ECT, double kHumid, double kJStock, ref double k)
+        { 
+            double denom = 0.0;
+
+            k = 5.87 * ECT * Math.Pow(2.0 * (L + B) , 0.4924) * Math.Pow(T, 0.5076);
+            k = k * kHumid * kJStock;
+        }
+        #endregion
+    }
+}

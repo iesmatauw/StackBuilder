cd9481a5f4ea45945e524a42217ef7d7fdd46c4a
fgasnier71_cp <SND\fgasnier71_cp>
2011-09-14 01:30:16 -0700

    + Added forgotten file DockContentStartPage.cs

---
+++ TreeDim.StackBuilder.Desktop/DockContentStartPage.cs
@@ -0,0 +1,28 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+using log4net;
+using TreeDim.StackBuilder.Desktop.Properties;
+#endregion
+
+namespace TreeDim.StackBuilder.Desktop
+{
+    public partial class DockContentStartPage : DockContent
+    {
+        public DockContentStartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DockContentStartPage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

d48934491e0faa7dfa8390546d60a0212a3ceddf
fgasnier71_cp <SND\fgasnier71_cp>
2011-10-19 03:25:44 -0700

    + Added comments

--- ExceptionReporter/Core/ExceptionReport.cs
+++ ExceptionReporter/Core/ExceptionReport.cs
@@ -16,25 +16,40 @@ namespace ExceptionReporting.Core
         {
             _reportString = stringBuilder;
         }
-
+        #region System.Object Overrides
+        /// <summary>
+        /// ToString()
+        /// </summary>
         public override string ToString()
         {
             return _reportString.ToString();
         }
-
+        /// <summary>
+        /// Returns a value indicating whether this instance is equal to
+        /// the specified object.
+        /// </summary>
+        /// <param name="obj">An object to compare to this instance.</param>
     	private bool Equals(ExceptionReport obj)
         {
             return Equals(obj._reportString.ToString(), _reportString.ToString());
         }
-
+        /// <summary>
+        /// Returns a value indicating whether this instance is equal to
+        /// the specified object.
+        /// </summary>
+        /// <param name="obj">An object to compare to this instance.</param>
         public override bool Equals(object obj)
         {
             return Equals((ExceptionReport) obj);
         }
-
+        /// <summary>
+        /// Returns the hashcode for this instance.
+        /// </summary>
+        /// <returns>A 32-bit signed integer hash code.</returns>
         public override int GetHashCode()
         {
             return _reportString.GetHashCode();
         }
+        #endregion
     }
 }

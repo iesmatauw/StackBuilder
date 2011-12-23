5851716ef6768db17ff2fe49be585433c3ab2878
fgasnier71_cp <SND\fgasnier71_cp>
2011-12-22 17:01:57 -0800

    + Added FormEditCardboardQualityData to edit cardboard qualities...

---
+++ TreeDim.EdgeCrushTest/FormEditCardboardQualityData.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace TreeDim.EdgeCrushTest
+{
+    partial class FormEditCardboardQualityData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormEditCardboardQualityData));
+            this.bnOk = new System.Windows.Forms.Button();
+            this.bnCancel = new System.Windows.Forms.Button();
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbProfile = new System.Windows.Forms.Label();
+            this.lbThickness = new System.Windows.Forms.Label();
+            this.lbECT = new System.Windows.Forms.Label();
+            this.lbRigidityX = new System.Windows.Forms.Label();
+            this.lbRigidityY = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.nudThickness = new System.Windows.Forms.NumericUpDown();
+            this.nudRigidityX = new System.Windows.Forms.NumericUpDown();
+            this.nudRigidityY = new System.Windows.Forms.NumericUpDown();
+            this.nudECT = new System.Windows.Forms.NumericUpDown();
+            this.lbThicknessUnit = new System.Windows.Forms.Label();
+            this.lbStiffnessXUnit = new System.Windows.Forms.Label();
+            this.lbStiffnessYUnit = new System.Windows.Forms.Label();
+            this.lbECTUnit = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudThickness)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRigidityX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRigidityY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudECT)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bnOk
+            // 
+            resources.ApplyResources(this.bnOk, "bnOk");
+            this.bnOk.Name = "bnOk";
+            this.bnOk.UseVisualStyleBackColor = true;
+            // 
+            // bnCancel
+            // 
+            resources.ApplyResources(this.bnCancel, "bnCancel");
+            this.bnCancel.Name = "bnCancel";
+            this.bnCancel.UseVisualStyleBackColor = true;
+            // 
+            // lbName
+            // 
+            resources.ApplyResources(this.lbName, "lbName");
+            this.lbName.Name = "lbName";
+            // 
+            // lbProfile
+            // 
+            resources.ApplyResources(this.lbProfile, "lbProfile");
+            this.lbProfile.Name = "lbProfile";
+            // 
+            // lbThickness
+            // 
+            resources.ApplyResources(this.lbThickness, "lbThickness");
+            this.lbThickness.Name = "lbThickness";
+            // 
+            // lbECT
+            // 
+            resources.ApplyResources(this.lbECT, "lbECT");
+            this.lbECT.Name = "lbECT";
+            // 
+            // lbRigidityX
+            // 
+            resources.ApplyResources(this.lbRigidityX, "lbRigidityX");
+            this.lbRigidityX.Name = "lbRigidityX";
+            // 
+            // lbRigidityY
+            // 
+            resources.ApplyResources(this.lbRigidityY, "lbRigidityY");
+            this.lbRigidityY.Name = "lbRigidityY";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // nudThickness
+            // 
+            resources.ApplyResources(this.nudThickness, "nudThickness");
+            this.nudThickness.DecimalPlaces = 2;
+            this.nudThickness.Name = "nudThickness";
+            // 
+            // nudRigidityX
+            // 
+            resources.ApplyResources(this.nudRigidityX, "nudRigidityX");
+            this.nudRigidityX.DecimalPlaces = 2;
+            this.nudRigidityX.Name = "nudRigidityX";
+            // 
+            // nudRigidityY
+            // 
+            resources.ApplyResources(this.nudRigidityY, "nudRigidityY");
+            this.nudRigidityY.DecimalPlaces = 2;
+            this.nudRigidityY.Name = "nudRigidityY";
+            // 
+            // nudECT
+            // 
+            resources.ApplyResources(this.nudECT, "nudECT");
+            this.nudECT.DecimalPlaces = 2;
+            this.nudECT.Name = "nudECT";
+            // 
+            // lbThicknessUnit
+            // 
+            resources.ApplyResources(this.lbThicknessUnit, "lbThicknessUnit");
+            this.lbThicknessUnit.Name = "lbThicknessUnit";
+            // 
+            // lbStiffnessXUnit
+            // 
+            resources.ApplyResources(this.lbStiffnessXUnit, "lbStiffnessXUnit");
+            this.lbStiffnessXUnit.Name = "lbStiffnessXUnit";
+            // 
+            // lbStiffnessYUnit
+            // 
+            resources.ApplyResources(this.lbStiffnessYUnit, "lbStiffnessYUnit");
+            this.lbStiffnessYUnit.Name = "lbStiffnessYUnit";
+            // 
+            // lbECTUnit
+            // 
+            resources.ApplyResources(this.lbECTUnit, "lbECTUnit");
+            this.lbECTUnit.Name = "lbECTUnit";
+            // 
+            // FormEditCardboardQualityData
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbECTUnit);
+            this.Controls.Add(this.lbStiffnessYUnit);
+            this.Controls.Add(this.lbStiffnessXUnit);
+            this.Controls.Add(this.lbThicknessUnit);
+            this.Controls.Add(this.nudECT);
+            this.Controls.Add(this.nudRigidityY);
+            this.Controls.Add(this.nudRigidityX);
+            this.Controls.Add(this.nudThickness);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbRigidityY);
+            this.Controls.Add(this.lbRigidityX);
+            this.Controls.Add(this.lbECT);
+            this.Controls.Add(this.lbThickness);
+            this.Controls.Add(this.lbProfile);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.bnCancel);
+            this.Controls.Add(this.bnOk);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormEditCardboardQualityData";
+            this.ShowIcon = false;
+            ((System.ComponentModel.ISupportInitialize)(this.nudThickness)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRigidityX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRigidityY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudECT)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnOk;
+        private System.Windows.Forms.Button bnCancel;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbProfile;
+        private System.Windows.Forms.Label lbThickness;
+        private System.Windows.Forms.Label lbECT;
+        private System.Windows.Forms.Label lbRigidityX;
+        private System.Windows.Forms.Label lbRigidityY;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NumericUpDown nudThickness;
+        private System.Windows.Forms.NumericUpDown nudRigidityX;
+        private System.Windows.Forms.NumericUpDown nudRigidityY;
+        private System.Windows.Forms.NumericUpDown nudECT;
+        private System.Windows.Forms.Label lbThicknessUnit;
+        private System.Windows.Forms.Label lbStiffnessXUnit;
+        private System.Windows.Forms.Label lbStiffnessYUnit;
+        private System.Windows.Forms.Label lbECTUnit;
+    }
+}
---
+++ TreeDim.EdgeCrushTest/FormEditCardboardQualityData.cs
@@ -0,0 +1,28 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+#endregion
+
+namespace TreeDim.EdgeCrushTest
+{
+    public partial class FormEditCardboardQualityData : Form
+    {
+        #region Data members
+        enum Mode
+        { MODE_CREATE
+            , MODE_MODIFY}
+        #endregion
+
+        #region Constructor
+        public FormEditCardboardQualityData()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}

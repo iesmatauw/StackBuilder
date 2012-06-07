191b01c9b1b8140e8eda74f798800c535306720d
fgasnier71_cp <SND\fgasnier71_cp>
2012-06-07 03:06:28 -0700

    + Moved installed binaries to bin directory + Implemented two separated samples

--- Samples/TestXmlFileProcessor.bat
+++ Samples/TestXmlFileProcessor.bat
@@ -1,3 +1,11 @@
-rmdir .\XmlFileProcessorOutput
-mkdir .\XmlFileProcessorOutput
-..\TreeDim.StackBuilder.XmlFileProcessor.exe /i ".\XmlFileProcessorInput.xml"
+REM *** Sample with case 440x280x290
+rmdir /s /q .\Output1
+mkdir .\Output1
+..\Bin\TreeDim.StackBuilder.XmlFileProcessor.exe /i ".\Input1.xml"
+
+REM *** Sample with bundle
+rmdir /s /q .\Output2
+mkdir .\Output2
+..\Bin\TreeDim.StackBuilder.XmlFileProcessor.exe /i ".\Input2.xml"
+
+PAUSE

c4ee04a7889b5ebe6e0613818b600d126e72ca93
fgasnier71_cp <SND\fgasnier71_cp>
2012-10-19 00:23:56 -0700

    + Added classes to handle cylinder...

---
+++ TreeDim.StackBuilder.Engine/CylinderSolver.cs
@@ -0,0 +1,15 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TreeDim.StackBuilder.Basics;
+#endregion
+
+namespace TreeDim.StackBuilder.Engine
+{
+    public interface ICylinderSolver
+    {
+        void ProcessAnalysis(PalletAnalysisCylinder truckAnalysis);
+    }
+}

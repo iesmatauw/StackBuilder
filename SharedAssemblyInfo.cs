7df6240c854e3f7a0bbc6714c88ee09c3b63f37f
fgasnier71 <fgasnier71@gmail.com>
2014-07-29 12:14:14 +0200

    Version 1.0.26.0
    * Removed version info from AssemblyInfo.cs for most projects (now in
    SharedAssemblyInfo.cs)
    * Corrected various typo in french translations,
    * Now allows maximum pallet length/Width to reach 20000, and weight
    10000...
    * Layer pattern "Spiral" can now be inverted (180°)

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.26.0")]
+[assembly: AssemblyFileVersion("1.0.26.0")]

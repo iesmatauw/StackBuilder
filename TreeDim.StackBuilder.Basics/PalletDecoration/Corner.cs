2bb005e9b8b06d9d6866a8cc1809c01942c243e3
François Gasnier <fgasnier71@gmail.com>
2014-12-18 07:40:08 +0100

    Added corner class...

---
+++ TreeDim.StackBuilder.Basics/PalletDecoration/Corner.cs
@@ -0,0 +1,53 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+using Sharp3D.Math.Core;
+#endregion
+
+namespace TreeDim.StackBuilder.Basics.PalletDecoration
+{
+    public class Corner : ItemBase
+    {
+        #region Data members
+        private double _width;
+        private Color _color;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Constructor 1
+        /// </summary>
+        /// <param name="document">Parent document</param>
+        public Corner(Document document)
+            : base(document)
+        { 
+        }
+        /// <summary>
+        /// Constructor 2
+        /// </summary>
+        /// <param name="document">Parent document</param>
+        /// <param name="width">Width</param>
+        /// <param name="color">Color</param>
+        public Corner(Document document, double width, Color color)
+            : base(document)
+        { 
+        }
+        #endregion
+
+        #region Properties
+        public double Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+        public Color Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+        #endregion
+    }
+}
